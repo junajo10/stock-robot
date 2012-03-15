76b46ed76964079b08d0d56fd6b52c4ea5cef1eb
erikramqvist <erikramqvist@gmail.com>
2012-03-15 20:36:54 +0100

    Added parse() function in AvanzaParser and uppdated the class a little

--- stockrobot/src/parser/AvanzaParser.java
+++ stockrobot/src/parser/AvanzaParser.java
@@ -11,16 +11,38 @@ import java.util.ArrayList;
  * 
  */
 public class AvanzaParser implements IParser {
-
+	boolean run = true;
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
+		run = true;
 		ArrayList<URL> avanzaURLList = new ArrayList<URL>();
+		ArrayList<String> stocks = new ArrayList<String>();
+		
+		while(true){
+			while(run){
+				Long timeBefore = System.currentTimeMillis();
+				for(URL url : avanzaURLList){
+					parse(url);
+				}
+				Long timeElapsed = System.currentTimeMillis() - timeBefore;
+				try {
+					Thread.sleep(60000-timeElapsed);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
 	}
 
+	/** Pause the parser until run() is called. */
 	@Override
 	public void hold() {
-		// TODO Implement a hold function.
+		run = false;
+	}
+	
+	/** Parses the given Avanza-URL
+	 * @param URL to be parsed */
+	public void parse(URL url){
 		
 	}
 
