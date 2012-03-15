98c09faff4a88dc72e6a549e83554582ea57b6fb
erikramqvist <erikramqvist@gmail.com>
2012-03-15 20:11:56 +0100

    Avanzaparser \n Added a start of Avanzapaser

---
+++ stockrobot/src/parser/AvanzaParser.java
@@ -0,0 +1,27 @@
+package parser;
+
+import java.net.URL;
+import java.util.ArrayList;
+
+/**
+ * @author Erik
+ *
+ * A parser made specifically for the Avanza homepage.
+ * 
+ * 
+ */
+public class AvanzaParser implements IParser {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		ArrayList<URL> avanzaURLList = new ArrayList<URL>();
+	}
+
+	@Override
+	public void hold() {
+		// TODO Implement a hold function.
+		
+	}
+
+}
