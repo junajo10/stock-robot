da31063b86f67fc5d1a7a756ed00a9007921e972
Kristian Sällberg <kristian@purestyle.se>
2012-03-18 12:08:13 +0100

    Added ConnectionCreator to simplify the creation of database connections. Also Added a new method to the interface IInjector, it fixes all market changes (for instance a company moving from small cap to mid cap) that might have happened over night to the database.

--- stockrobot/src/scraping/database/IInjector.java
+++ stockrobot/src/scraping/database/IInjector.java
@@ -14,4 +14,15 @@ public interface IInjector {
 	 * @return
 	 */
 	public boolean injectStockData( ParserStock[] s );
+	
+	/**
+	 * Given an array with stock data formatted according to PriceDataRepresentation
+	 * try to update the stock market if it doesn't match the content of s
+	 * 
+	 * Returns true if everything was successful, and false if an error occurred. 
+	 * 
+	 * @param s
+	 * @return
+	 */
+	public boolean updateAllMarkets( ParserStock[] s );
 }
