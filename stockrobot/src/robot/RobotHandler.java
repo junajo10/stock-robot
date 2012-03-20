fafb7aecae56a7179212730cd2e933ab88ee4229
Mattias <mattias.markehed@gmail.com>
2012-03-20 19:06:59 +0100

    Added RobotHandler that takes care of running algorithms frequently
    Added the class RobotHandler who should take care of running all the
    algorithms loaded in the portfolios at appropriate times. 
    This will take some load of the current class PortfolioHandler.java make
    better separation of duty.

---
+++ stockrobot/src/robot/RobotHandler.java
@@ -0,0 +1,14 @@
+package robot;
+
+/**
+ * @author Mattias Markehed
+ * mattias.markehed@gmail.com
+ *
+ * filename: RobotHandler.java
+ * Description:
+ * RobotHandler takes care of running the stock algorithms 
+ * on a frequent basis. 
+ */
+public class RobotHandler {
+
+}
