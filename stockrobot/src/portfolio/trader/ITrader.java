3c8440f637a28bf5a13b1234e075fe5a464f82f6
Mattias <mattias.markehed@gmail.com>
2012-03-20 18:59:48 +0100

    Added class description for ITrader

--- stockrobot/src/portfolio/trader/ITrader.java
+++ stockrobot/src/portfolio/trader/ITrader.java
@@ -1,5 +1,16 @@
 package portfolio.trader;
 
+/**
+ * @author Mattias Markehed
+ * mattias.markehed@gmail.com
+ *
+ * filename: ITrader.java
+ * Description:
+ * Interface for buying and selling stock.
+ * This will be used by the algorithms to for 
+ * buying new stock or selling old one.
+ */
 public interface ITrader {
 
+	//TODO Add buy and sell methods.  
 }
