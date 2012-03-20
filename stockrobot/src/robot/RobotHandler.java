b997096f93a3f24d716e0b211a9dd5a0a6bcd5a0
Mattias <mattias.markehed@gmail.com>
2012-03-20 22:48:39 +0100

    RobotHandler: Added start and stop methods for the handler
    RobotHandler: Add start and stop methods for RobotHandler.

--- stockrobot/src/robot/RobotHandler.java
+++ stockrobot/src/robot/RobotHandler.java
@@ -1,5 +1,7 @@
 package robot;
 
+import portfolio.IPortfolioHandler;
+
 /**
  * @author Mattias Markehed
  * mattias.markehed@gmail.com
@@ -11,4 +13,53 @@ package robot;
  */
 public class RobotHandler {
 
+	//Used to retrieve the algorithms and portfolios that are used
+	private IPortfolioHandler portfolioHandler;
+	
+	
+	private boolean isRunning = false;
+	
+	//TODO Implement a shedueler for the handler
+	
+	/**
+	 * Create a handler thats runs each algorithms in the
+	 * portfolios frequently.
+	 * 
+	 * @param portfolioHandler where to get used portfolios
+	 */
+	public RobotHandler(IPortfolioHandler portfolioHandler){
+		
+		this.portfolioHandler = portfolioHandler;
+	}
+
+	/**
+	 * 
+	 * Start the handler. Nothing happens if 
+	 * handler is already running.
+	 * 
+	 * @return true if started
+	 */
+	public void startHandler(){
+		
+		if(!isRunning){
+			isRunning = true;
+		}
+	}
+
+	/**
+	 * Stop the handler after current run of
+	 * algorithms are finished.
+	 */
+	public void stopHandler(){
+		isRunning = false;
+	}
+	
+	/**
+	 * The loop that starts and runs the algorithms
+	 * for each portfolio.
+	 */
+	private void runLoop(){
+		
+	}
+	
 }
