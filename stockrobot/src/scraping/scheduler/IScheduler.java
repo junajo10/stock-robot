c0292092afc3074be6fa197d3cca043221b0d116
Kristian Sällberg <kristian@purestyle.se>
2012-03-21 22:20:02 +0100

    Added methods for setting start and end of the day. Removed pauseRunner as this class should NOT be able to pause any runner, let alone know of the existence of any runner. Added a boolean value as parameter to ignoreTime so it can be set both ways, on and off.

--- stockrobot/src/scraping/scheduler/IScheduler.java
+++ stockrobot/src/scraping/scheduler/IScheduler.java
@@ -1,13 +1,44 @@
 package scraping.scheduler;
 
+import java.sql.Date;
+
 /**
  *
  * Abstract interface for the Scheduler.
- * @author Erik
+ * @author Erik,kristian
  *
  */
 public interface IScheduler {
+	
+	/**
+	 * When called, shouldRun will return true or false depending on how
+	 * 
+	 * @return
+	 */
 	public boolean shouldRun();
-	public void ignoreTime();
-	public boolean pauseParser();
+	
+	/**
+	 * A date that represents what the start time for a specific data source should be
+	 * 
+	 * @param start
+	 * @return
+	 */
+	public boolean setStartTime( Date start );
+	
+	/**
+	 * A date that represents what the end time for a specific data source should be
+	 * 
+	 * @param end
+	 * @return
+	 */
+	public boolean setEndTime( Date end );
+	
+	/**
+	 * Ignore time, can be set to true or false,
+	 * true will make the shouldRun method to always return true
+	 * false will make shouldRun to return true according to time of day 
+	 * 
+	 * @param ignore
+	 */
+	public void ignoreTime(boolean ignore);
 }
