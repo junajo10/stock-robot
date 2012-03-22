359b2659a8caaaa9e845199e585ea62b2c36d032
Daniel <danielmalmqvists@gmail.com>
2012-03-22 15:51:59 +0100

    Added unique constraint to StockPrices

--- stockrobot/src/database/jpa/tables/StockPrices.java
+++ stockrobot/src/database/jpa/tables/StockPrices.java
@@ -2,16 +2,17 @@ package database.jpa.tables;
 
 import java.util.Date;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
 
 @Entity
-@Table(name="StockPrices")
+@Table(name="StockPrices", uniqueConstraints=@UniqueConstraint(columnNames={"time", "stockName"}))
 public final class StockPrices {
 	@Id
 	@GeneratedValue
@@ -66,6 +67,6 @@ public final class StockPrices {
 		return time;
 	}
 	public String toString() {
-		return stockName.getName() + " | " + buy + " | " + sell;
+		return stockName.getName() + " | Buy: " + buy + " | Sell: " + sell + " | Market: " + stockName.getMarket();
 	}
 }
