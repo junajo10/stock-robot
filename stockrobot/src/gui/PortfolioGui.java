1130c38a656a65b1173d3b26482734f68cd43ae1
Mattias <mattias.markehed@gmail.com>
2012-03-23 15:32:49 +0100

    Minor Gui changes

--- stockrobot/src/gui/PortfolioGui.java
+++ stockrobot/src/gui/PortfolioGui.java
@@ -1,6 +1,7 @@
 package gui;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
@@ -20,6 +21,9 @@ import javax.swing.JScrollBar;
 import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
+import java.awt.Component;
+import javax.swing.Box;
+import java.awt.Button;
 
 public class PortfolioGui extends JFrame {
 
@@ -45,36 +49,25 @@ public class PortfolioGui extends JFrame {
 	 * Create the frame.
 	 */
 	public PortfolioGui() {
+		setResizable(false);
 		setForeground(Color.WHITE);
 		setBackground(Color.BLACK);
 		setTitle("Portfolio");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 262, 270);
+		setBounds(100, 100, 300, 281);
 		contentPane = new JPanel();
 		contentPane.setBackground(Color.WHITE);
 		setContentPane(contentPane);
-		
-		JPanel pnl_BoxContainers = new JPanel();
-		FlowLayout flowLayout_4 = (FlowLayout) pnl_BoxContainers.getLayout();
-		flowLayout_4.setAlignment(FlowLayout.LEFT);
-		pnl_BoxContainers.setBackground(null);
+		JPanel pnl_BoxContainer = new JPanel();
+		pnl_BoxContainer.setBackground(new Color(245, 245, 245));
+		contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.Y_AXIS));
 		
 		PortfolioContainer pnl_menu = new PortfolioContainer();
+		pnl_menu.setMinimumSize(new Dimension(100, 35));
+		pnl_menu.setPreferredSize(new Dimension(300, 35));
+		pnl_menu.setMaximumSize(new Dimension(300,35));
 		FlowLayout flowLayout_3 = (FlowLayout) pnl_menu.getLayout();
 		flowLayout_3.setAlignment(FlowLayout.RIGHT);
-		GroupLayout gl_contentPane = new GroupLayout(contentPane);
-		gl_contentPane.setHorizontalGroup(
-			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addComponent(pnl_menu, GroupLayout.DEFAULT_SIZE, 252, Short.MAX_VALUE)
-				.addComponent(pnl_BoxContainers, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-		);
-		gl_contentPane.setVerticalGroup(
-			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addComponent(pnl_menu, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(pnl_BoxContainers, GroupLayout.DEFAULT_SIZE, 201, Short.MAX_VALUE))
-		);
 		
 		JLabel lbl_Portfolio = new JLabel("Portfolio");
 		pnl_menu.add(lbl_Portfolio);
@@ -82,10 +75,51 @@ public class PortfolioGui extends JFrame {
 		JComboBox comboBox = new JComboBox();
 		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Portfolio1", "Portfolio2", "Portfolio3"}));
 		pnl_menu.add(comboBox);
+		contentPane.add(pnl_menu);
+		contentPane.add(pnl_BoxContainer);
+		pnl_BoxContainer.setLayout(new FlowLayout(FlowLayout.LEFT, 5, 5));
+		
+		PortfolioContainer pnl_AlgorithmContainer = new PortfolioContainer();
+		pnl_BoxContainer.add(pnl_AlgorithmContainer);
+		pnl_AlgorithmContainer.setLayout(new BoxLayout(pnl_AlgorithmContainer, BoxLayout.Y_AXIS));
+		
+		Component horizontalStrut_1 = Box.createHorizontalStrut(200);
+		pnl_AlgorithmContainer.add(horizontalStrut_1);
+		
+		JPanel pnl_AlgorithmPanel = new JPanel();
+		FlowLayout flowLayout_5 = (FlowLayout) pnl_AlgorithmPanel.getLayout();
+		flowLayout_5.setAlignment(FlowLayout.LEFT);
+		pnl_AlgorithmPanel.setBackground((Color) null);
+		pnl_AlgorithmContainer.add(pnl_AlgorithmPanel);
+		
+		JLabel lbl_Algorithm = new JLabel("Algorithms");
+		lbl_Algorithm.setHorizontalAlignment(SwingConstants.LEFT);
+		pnl_AlgorithmPanel.add(lbl_Algorithm);
+		
+		JPanel pnl_AlgorithmName = new JPanel();
+		FlowLayout flowLayout_6 = (FlowLayout) pnl_AlgorithmName.getLayout();
+		flowLayout_6.setAlignment(FlowLayout.LEFT);
+		pnl_AlgorithmContainer.add(pnl_AlgorithmName);
+		pnl_AlgorithmName.setBackground((Color) null);
+		
+		JLabel lbl_AlgorithmName = new JLabel("None");
+		lbl_AlgorithmName.setHorizontalAlignment(SwingConstants.LEFT);
+		pnl_AlgorithmName.add(lbl_AlgorithmName);
+		
+		JPanel pnl_AlgorithmChange = new JPanel();
+		pnl_AlgorithmContainer.add(pnl_AlgorithmChange);
+		pnl_AlgorithmChange.setBackground((Color) null);
+		
+		JButton btn_AlgorithmChange2 = new JButton("Change");
+		btn_AlgorithmChange2.setBackground(Color.WHITE);
+		pnl_AlgorithmChange.add(btn_AlgorithmChange2);
 		
 		PortfolioContainer pnl_BalanceContainer = new PortfolioContainer();
-		pnl_BoxContainers.add(pnl_BalanceContainer);
+		pnl_BoxContainer.add(pnl_BalanceContainer);
 		pnl_BalanceContainer.setLayout(new BoxLayout(pnl_BalanceContainer, BoxLayout.Y_AXIS));
+		
+		Component horizontalStrut = Box.createHorizontalStrut(200);
+		pnl_BalanceContainer.add(horizontalStrut);
 		
 		JPanel pnl_BalancePanel = new JPanel();
 		FlowLayout flowLayout_2 = (FlowLayout) pnl_BalancePanel.getLayout();
@@ -93,6 +127,7 @@ public class PortfolioGui extends JFrame {
 		pnl_BalanceContainer.add(pnl_BalancePanel);
 		
 		JLabel lbl_Balance = new JLabel("Balance");
+		lbl_Balance.setHorizontalAlignment(SwingConstants.LEFT);
 		pnl_BalancePanel.add(lbl_Balance);
 		pnl_BalancePanel.setBackground(null);
 		
@@ -125,8 +160,12 @@ public class PortfolioGui extends JFrame {
 		pnl_BalanceHistory.setBackground(null);
 		
 		JButton btn_balanceHistory = new JButton("History");
+		btn_balanceHistory.setForeground(Color.BLACK);
 		pnl_BalanceHistory.add(btn_balanceHistory);
 		btn_balanceHistory.setBackground(Color.WHITE);
-		contentPane.setLayout(gl_contentPane);
+		
+		JButton button = new JButton("Change");
+		button.setBackground(Color.WHITE);
+		btn_balanceHistory.setBackground(Color.WHITE);
 	}
 }
