eda840d4f2d1b8684d8be8c72cb48dba92829158
Daniel <danielmalmqvists@gmail.com>
2012-03-23 14:03:05 +0100

    More comments.

--- stockrobot/src/generic/Pair.java
+++ stockrobot/src/generic/Pair.java
@@ -1,5 +1,13 @@
 package generic;
 
+/**
+ * @author Daniel
+ *
+ * @param <L> The left object type
+ * @param <R> The right object type
+ * 
+ * Just a simple tuple class
+ */
 public class Pair<L, R> {
 	private L left;
 	private R right;
@@ -8,9 +16,15 @@ public class Pair<L, R> {
 		this.left = left;
 		this.right = right;
 	}
+	/**
+	 * @return Returns the left side of the tuple
+	 */
 	public L getLeft() {
 		return left;
 	}
+	/**
+	 * @return Returns the right side of the tuple
+	 */
 	public R getRight() {
 		return right;
 	}
