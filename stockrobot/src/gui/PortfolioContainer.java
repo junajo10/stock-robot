397f6e337acf5fcbdb6077ad971dc0cc33c0dfa5
Mattias <mattias.markehed@gmail.com>
2012-03-23 15:31:42 +0100

    Minor estetical changes made to GUI

--- stockrobot/src/gui/PortfolioContainer.java
+++ stockrobot/src/gui/PortfolioContainer.java
@@ -1,6 +1,8 @@
 package gui;
 
 import javax.swing.JPanel;
+import java.awt.Color;
+import javax.swing.border.LineBorder;
 
 public class PortfolioContainer extends JPanel {
 
@@ -8,6 +10,8 @@ public class PortfolioContainer extends JPanel {
 	 * Create the panel.
 	 */
 	public PortfolioContainer() {
+		setBorder(new LineBorder(new Color(0, 0, 0)));
+		setBackground(Color.LIGHT_GRAY);
 
 	}
 
