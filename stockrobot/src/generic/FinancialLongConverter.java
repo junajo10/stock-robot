cf25e0f44b555073c39907d6e83764273d5e29be
Daniel <danielmalmqvists@gmail.com>
2012-03-24 20:54:59 +0100

    Changed toStringTwoDecimalPoints to use DecimalFormat instead.

--- stockrobot/src/generic/FinancialLongConverter.java
+++ stockrobot/src/generic/FinancialLongConverter.java
@@ -1,5 +1,7 @@
 package generic;
 
+import java.text.DecimalFormat;
+
 /**
  * 
  * FinancialLongConverter
@@ -64,25 +66,11 @@ public class FinancialLongConverter {
 		
 		return Long.parseLong( numbers + decimals );
 	}
-	private static String negativeToStringTwoDecimalPoints(long input) {
-		return "-" + toStringTwoDecimalPoints(Math.abs(input));
+	public static double toDouble(long input) {
+		return input/(Math.pow(10, decimalLength));
 	}
 	public static String toStringTwoDecimalPoints(long input) {
-		if (input < 0)
-			return negativeToStringTwoDecimalPoints(input);
-		
-		String s = "" + input;
-		
-		if (s.length()-1 > decimalLength-2) {
-			return s.substring(0, s.length()-decimalLength) + "." + 
-					s.substring(s.length()-decimalLength, s.length()-decimalLength+2);
-		}
-		else {
-			long decimals = (long) (input/(Math.pow(10, decimalLength-2)));
-			
-			if (decimals < 10)
-					return "0.0" + decimals;
-			return "0." + decimals;
-		}
+		DecimalFormat df = new DecimalFormat("#.##");
+		return df.format(toDouble(input));
 	}
 }
