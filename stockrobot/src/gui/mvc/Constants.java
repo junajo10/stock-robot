e144a47e7516b09e4878f5eccc0c6e9251072abb
Mattias <mattias.markehed@gmail.com>
2012-03-24 17:56:09 +0100

    Added GUI constants. that wasnt added in last commit

---
+++ stockrobot/src/gui/mvc/Constants.java
@@ -0,0 +1,19 @@
+package gui.mvc;
+
+/**
+ * @author Mattias Markehed
+ * mattias.markehed@gmail.com
+ *
+ * filename: Constants.java
+ * Description:
+ * The constants class holds constats that is used by the gui
+ */
+public class Constants {
+
+	//The type of messages that can be sent by a IEvent
+	public class EVENT_TYPE{
+		
+		public static final String BUY_STOCK = "BUY_STOCK";
+		public static final String SELL_STOCK = "SELL_STOCK";
+	}
+}
