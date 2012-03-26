4c4e0a313b56d984acb56cab5bda6a7892cfabe8
erikramqvist <erikramqvist@gmail.com>
2012-03-26 17:34:53 +0200

    Added method newStockData() in Reciever. Method returns true if theres new stock data.

--- stockrobot/src/scraping/connect/Reciever.java
+++ stockrobot/src/scraping/connect/Reciever.java
@@ -9,8 +9,10 @@ import java.net.UnknownHostException;
 
 public class Reciever implements Runnable {
 
+	private boolean newData;
 	/**
 	 * Test class to see if it recieves data from Connector class.
+	 * Recieves message from Harvester saying that new data is available.
 	 * <p>
 	 * Part of this class should be somewhere in the algorithms package.
 	 * <p>
@@ -24,7 +26,8 @@ public class Reciever implements Runnable {
 			while(true){
 				Socket newDataSocket = recieve.accept();
 				//BufferedReader fromHarvester = new BufferedReader(new InputStreamReader(newDataSocket.getInputStream()));
-				//Send ping upwards saying new data is available.
+				//Send ping upwards saying that new data is available.
+				newData = true;
 				System.out.println("Have recieved data!");
 				newDataSocket.close();
 			}
@@ -35,5 +38,18 @@ public class Reciever implements Runnable {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	
+	/**
+	 * Checks if new stockdata is available.
+	 * @return true if new stock data is available.
+	 * otherwise false
+	 */
+	public boolean newStockData(){
+		if(newData){
+			newData = false;
+			return true;
+		}
+		return false;
 	}
 }
