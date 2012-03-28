537d5dce79ace18258af6ac12fd49f76aacbdc5b
Daniel <danielmalmqvists@gmail.com>
2012-03-28 03:39:17 +0200

    Minor improvments to the distribution policy.

--- stockrobot/src/database/jpa/JPADistributionPolicy.java
+++ stockrobot/src/database/jpa/JPADistributionPolicy.java
@@ -4,13 +4,8 @@ import java.util.List;
 
 import org.apache.openjpa.slice.DistributionPolicy;
 
-import database.jpa.tables.AlgorithmEntitys;
-import database.jpa.tables.PortfolioEntitys;
-import database.jpa.tables.PortfolioHistory;
-import database.jpa.tables.PortfolioInvestment;
 import database.jpa.tables.StockNames;
 import database.jpa.tables.StockPrices;
-import database.jpa.tables.StocksToWatch;
 
 /**
  * @author Daniel
@@ -23,23 +18,11 @@ public class JPADistributionPolicy implements DistributionPolicy {
 	public static String stockDatabase = "Stocks";
 	
     public String distribute(Object p, List<String> slices, Object context) {
-    	
-		if (p instanceof AlgorithmEntitys)
-			return portfolioDatabase;
-		else if (p instanceof PortfolioEntitys)
-			return portfolioDatabase;
-		else if (p instanceof PortfolioHistory)
-			return portfolioDatabase;
-		else if (p instanceof PortfolioInvestment)
-			return portfolioDatabase;
-		else if (p instanceof StocksToWatch)
-			return portfolioDatabase;
-		
-		else if (p instanceof StockNames)
+    	if (p instanceof StockNames)
 			return stockDatabase;
 		else if (p instanceof StockPrices)
 			return stockDatabase;
 		
-		throw new IllegalArgumentException("Missed object " + p);
+		return portfolioDatabase;
 	}
 }
