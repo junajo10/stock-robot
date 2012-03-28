84a1c16520c593a66866a0b45d475c42d5dc3917
Kristian Sällberg <kristian@purestyle.se>
2012-03-28 17:25:50 +0200

    Added a window title to this frame

--- stockrobot/src/gui/StockInfoGUI.java
+++ stockrobot/src/gui/StockInfoGUI.java
@@ -1,17 +1,33 @@
 package gui;
 
+import gui.components.GUIFactory;
 import gui.view.StockTableView;
 
 import javax.swing.JFrame;
 
+/**
+ * Class to wrap the Stock Table View and possibly other related classes
+ * 
+ * @author kristian
+ *
+ */
 public class StockInfoGUI extends JFrame {
 	
-	StockTableView view = new StockTableView();
+	private static final String WINDOW_TITLE = "All publicly traded companies currently recorded in the database.";
+	
+	private						StockTableView view;
 	
 	public StockInfoGUI() {
 		
+		GUIFactory guiFactory = new GUIFactory();
+		
+		view = (StockTableView) guiFactory.getStockTableView();
+		
 		setBounds( 100, 200, 800, 600 );
 		add( view );
+		
+		//Set title
+		setTitle( WINDOW_TITLE );
 		
 		this.setVisible( true );
 	}
