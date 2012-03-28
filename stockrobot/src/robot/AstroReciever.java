4ab42b325da7c17afda3ee6de04e6ee4de566fa1
erikramqvist <erikramqvist@gmail.com>
2012-03-28 17:25:50 +0200

    Cleaned up comments and code on AstroReciever.java

--- stockrobot/src/robot/AstroReciever.java
+++ stockrobot/src/robot/AstroReciever.java
@@ -1,51 +1,44 @@
 package robot;
 
-import generic.Log;
-
 import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.UnknownHostException;
 
 /**
  * Class for receiving notifications that there's new stock data.
  * <p>
- * Receives messages from Connector class on port 45000.
+ * Receives a message from Connector class on port 45000,
+ * containing the time in ms on the latest parsing.
  * <p>
  * @author Erik
  *
  */
 public class AstroReciever {
-	private Socket sendRefresh;
-	private boolean newData;
-	private final int secretKey = 19286;
-	private String stockTime = "";
 	
-	/** Code should be somewhere in Astro.java
-	 * <p>
-	 * AstroReciever should be run as a thread.
-	 * 
-	 */
+	private final int PORT_NR = 45000;
+	private final String SERVER_ADRESS = "localhost";
+	private String stockTime = System.currentTimeMillis() + "";
 
 	/**
-	 * Sends a message to the robot saying that new data is available.
+	 * Asks Connector to get the time of the latest parsing in ms.
+	 * <p>
+	 * 
 	 * @return true if it was sent successfully.
 	 */
 	public boolean newData() {
 		DataOutputStream outToServer;
 		DataInputStream inFromServer;
+		Socket serverSocket;
 		try {
-			//TODO: change "localhost" into an correct address,
-			sendRefresh = new Socket("localhost", 45000);
-			BufferedReader fromServer = new BufferedReader(new InputStreamReader(sendRefresh.getInputStream()));
+			serverSocket = new Socket(SERVER_ADRESS, PORT_NR);
+			BufferedReader fromServer = new BufferedReader(new InputStreamReader(serverSocket.getInputStream()));
 			while(!fromServer.ready()){}
 			String latestStocks = fromServer.readLine();
 			fromServer.close();
-			sendRefresh.close();
+			serverSocket.close();
 			if(!stockTime.equals(latestStocks)){
 				stockTime = latestStocks;
 				return true;
@@ -62,6 +55,7 @@ public class AstroReciever {
 	 * Method only for testing!
 	 * <p>
 	 * Not to be included in final version.
+	 * <p>
 	 */
 	public static void main(String[] args){
 		AstroReciever rec = new AstroReciever();
@@ -79,21 +73,4 @@ public class AstroReciever {
 			System.out.println("New data? " + rec.newData());
 		} 			
 	 }
-	
-	/**
-	 * Checks if new stock data is available.
-	 * <p>
-	 * Thread safe method.
-	 * <P>
-	 * @return true if new stock data is available.
-	 * <p>
-	 * Otherwise false.
-	 */
-	public boolean newStockData(){
-		if(newData){
-			newData = false;
-			return true;
-		}
-		return false;
-	}
 }
