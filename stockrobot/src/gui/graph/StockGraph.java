5ae4577903315f2751037907639ac31d55365a50
erikramqvist <erikramqvist@gmail.com>
2012-03-30 09:31:17 +0200

    Updated StockGraph to work with real data, instead of fictional data.

--- stockrobot/src/gui/graph/StockGraph.java
+++ stockrobot/src/gui/graph/StockGraph.java
@@ -1,5 +1,10 @@
 package gui.graph;
 
+import generic.FinancialLongConverter;
+
+import java.util.Date;
+import java.util.List;
+
 import javax.swing.JPanel;
 
 import javax.swing.JFrame;
@@ -10,31 +15,48 @@ import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.PiePlot3D;
 import org.jfree.data.general.DefaultPieDataset;
 import org.jfree.data.general.PieDataset;
+import org.jfree.data.time.Minute;
 import org.jfree.data.time.Month;
 import org.jfree.data.time.TimeSeries;
 import org.jfree.data.time.TimeSeriesCollection;
 import org.jfree.util.Rotation;
+
+import database.jpa.IJPAHelper;
+import database.jpa.JPAHelper;
+import database.jpa.tables.StockNames;
+import database.jpa.tables.StockPrices;
 
 /**
  * Example of an simple JFreeChart time-graph of an fictional stock.
  * 
  * For learning JFreeChart only.
  * 
+ * TODO: Implement MVC-model for this class.
+ * 
  * @author Erik
  *
  */
 public class StockGraph extends JFrame {
+	IJPAHelper jpa;
 
 	private static final long serialVersionUID = 1L;
 
 	public StockGraph() {
         super("Stock data.");
+        jpa = JPAHelper.getInstance();
+        List<StockPrices> stocksPrice = jpa.getAllStockPrices();
+        
+        StockPrices stock = stocksPrice.get(0);
+        StockNames stockname = stock.getStockName();
+
+
+        
 
         JFreeChart chart = ChartFactory.createTimeSeriesChart(
-        		"Stock price of H&M and Boliden.", // title
+        		"Stock price of "+ stockname.getName(), // title
         		"Date", // x-axis label
         		"Price Per Unit", // y-axis label
-        		getTimeSerie(), // data
+        		getStockHistory(stockname), // data
         		true, // create legend?
         		true, // generate tooltips?
         		false // generate URLs?
@@ -45,6 +67,18 @@ public class StockGraph extends JFrame {
         setContentPane(chartPanel);
 
     }
+	private TimeSeriesCollection getStockHistory(StockNames name){
+    	TimeSeries serie = new TimeSeries("Boliden B", Minute.class);
+        List<StockPrices> priceList = jpa.getPricesForStock(name);
+        for(StockPrices stock : priceList){
+        	Date date = stock.getTime();
+        	serie.addOrUpdate(new Minute(stock.getTime().getMinutes(), stock.getTime().getHours(), date.getDay(), date.getMonth(), date.getYear()+1900), FinancialLongConverter.toDouble(stock.getBuy()));
+        }
+    	TimeSeriesCollection dataset = new TimeSeriesCollection();
+    	dataset.addSeries(serie);
+    	
+    	return dataset;
+	}
     
     private TimeSeriesCollection getTimeSerie(){
     	TimeSeries s1 = new TimeSeries("Boliden B", Month.class);
