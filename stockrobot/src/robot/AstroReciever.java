0083826dff65ccb17ad4352dbfe5f2420ee1cb61
erikramqvist <erikramqvist@gmail.com>
2012-04-01 12:58:35 +0200

    Changed the design of AstroReciever.

--- stockrobot/src/robot/AstroReciever.java
+++ stockrobot/src/robot/AstroReciever.java
@@ -6,6 +6,7 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 /**
  * Class for receiving notifications that there's new stock data.
@@ -20,7 +21,7 @@ public class AstroReciever {
 	
 	private final int PORT_NR = 45000;
 	private final String SERVER_ADRESS = "localhost";
-	private String stockTime = System.currentTimeMillis() + "";
+	private boolean newData = false;
 
 	/**
 	 * Asks Connector to get the time of the latest parsing in ms.
@@ -29,35 +30,68 @@ public class AstroReciever {
 	 * @return true if it was sent successfully.
 	 */
 	public boolean newData() {
-		DataOutputStream outToServer;
-		DataInputStream inFromServer;
-		Socket serverSocket;
-		try {
-			serverSocket = new Socket(SERVER_ADRESS, PORT_NR);
-			BufferedReader fromServer = new BufferedReader(new InputStreamReader(serverSocket.getInputStream()));
-			while(!fromServer.ready()){}
-			String latestStocks = fromServer.readLine();
-			fromServer.close();
-			serverSocket.close();
-			if(!stockTime.equals(latestStocks)){
-				stockTime = latestStocks;
-				return true;
-			}
-			return false;
 
-		} catch (IOException e) {
-			e.printStackTrace();
+		if (newData) {
+			newData = false;
+			return true;
 		}
 		return false;
+
+	}
+	
+	private class AstroClient implements Runnable {
+		private Socket serverSocket;
+		DataOutputStream outToServer;
+		DataInputStream inFromServer;
+		@Override
+		public void run() {
+			connect();
+			while(true){
+				try {
+					BufferedReader fromServer = new BufferedReader(new InputStreamReader(serverSocket.getInputStream()));
+					while(!fromServer.ready()){}
+					String latestStocks = fromServer.readLine();
+					newData = true;
+					fromServer.close();
+					serverSocket.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					System.err.println("Error: Connection problems, retrying...");
+					connect();
+				}
+			}
+			
+			
+		}
+		
+		public boolean connect(){
+			try {
+				serverSocket = new Socket(SERVER_ADRESS, PORT_NR);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				try {
+					Thread.sleep(2000);
+				} catch (InterruptedException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				System.err.println("Error: Cant connect to host, reconnecting...");
+				connect();
+			}
+
+			return true;
+		}
+		
 	}
 
 	/**
 	 * Method only for testing!
+	 * Change PORT_NR and address to the correct value.
 	 * <p>
 	 * Not to be included in final version.
 	 * <p>
 	 */
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		AstroReciever rec = new AstroReciever();
 		try {
 			Thread.sleep(2000);
