fd89beecd524c0801b5a118766890b2296ac2e8c
Mattias <mattias.markehed@gmail.com>
2012-04-19 23:03:30 +0200

    Added special itembox used for portfolio

---
+++ stockrobot/src/gui/components/Item_cmb_Portfolio.java
@@ -0,0 +1,30 @@
+package gui.components;
+
+import portfolio.IPortfolio;
+
+/**
+ * @author Mattias Markehed
+ * mattias.markehed@gmail.com
+ *
+ * filename: PortfolioGui.java
+ * Description:
+ * Item_cmb_Portfolio is used as an adapter for the mapping IPortfolios
+ * to the combo box containing portfolios
+ */
+public class Item_cmb_Portfolio{
+	
+	private IPortfolio portfolio;
+	
+	public Item_cmb_Portfolio(IPortfolio portfolio){
+		this.portfolio = portfolio;
+	}
+	
+	public IPortfolio getPortfolio(){
+		return portfolio;
+	}
+	
+	@Override
+	public String toString(){
+		return portfolio.getName();
+	}
+}
