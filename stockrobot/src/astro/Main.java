0ae6a310ad6836766728add004078cf9a1ff65d5
Kristian Sällberg <kristian@purestyle.se>
2012-05-06 16:41:17 +0200

    Test commit

--- stockrobot/src/astro/Main.java
+++ stockrobot/src/astro/Main.java
@@ -9,6 +9,7 @@ import controller.gui.StartController;
 public class Main {
 
 	public static void main(String args[]) {
+		
 		IController startController = new StartController();
 		
 		IController astroController = new AstroController();
