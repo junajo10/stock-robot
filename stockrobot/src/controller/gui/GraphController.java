7377768b1e37a81d39ad84809eb74fae155c36de
Kristian Sällberg <kristian@purestyle.se>
2012-05-06 20:22:40 +0200

    Added demo case for how to use defineSubControllers

--- stockrobot/src/controller/gui/GraphController.java
+++ stockrobot/src/controller/gui/GraphController.java
@@ -2,6 +2,8 @@ package controller.gui;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.util.ArrayList;
 import java.util.List;
 
 import model.database.jpa.IJPAHelper;
@@ -13,6 +15,8 @@ import org.jfree.data.time.Minute;
 import org.jfree.data.time.TimeSeries;
 
 import utils.global.FinancialLongConverter;
+import utils.global.Log;
+import utils.global.Pair;
 import view.graph.GraphView;
 
 /**
@@ -21,46 +25,77 @@ import view.graph.GraphView;
  * @author kristian
  *
  */
-public class GraphController {
+public class GraphController implements IController {
+	
+	private static final String CLASS_NAME = "GraphController";
+	private static final String BIND_GRAPH_VIEW = "bindGraphView";
+	
+	private static final String WINDOW_TITLE = "Stock prices on a graph";
+	
+	
 	
 	//Keep a reference of the view to be able to insert and read stuff from it
-	GraphView _view;
+	private GraphView _view;
+	private List<Pair<String, ActionListener>> retList;
 	
-	/**
-	 * Generate a listener that looks into the model, searches for the wanted stock, and collects all of it's prices recorded
-	 * 
-	 * @return
-	 */
-	public ActionListener bindAddStock() {
+	public GraphView getView() { return _view; }
+
+	public GraphController() {
+		
+		retList = new ArrayList<Pair<String,ActionListener>>();
+	}
+	
+	@Override
+	public void propertyChange(PropertyChangeEvent arg0) {
+		
+		Log.instance().log(Log.TAG.VERY_VERBOSE, "GraphView: Property Change");
+	}
+
+	@Override
+	public void display(Object model) {
+		
+		_view = new GraphView( WINDOW_TITLE );
+		_view.bindAddStockButton( getActionListeners().get(0).getRight() );
+		
+		_view.init();
+	}
+
+	@Override
+	public void cleanup() {
+		
+		_view = null;
+	}
+
+	public ActionListener bindGraphView() {
 		
 		return new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed( ActionEvent arg0 ) {
-				
+		
 				//Sign up for the jpaHelper
 				IJPAHelper jpaHelper = JPAHelper.getInstance();
 				List<StockNames> nameList = jpaHelper.getAllStockNames();
-				
+		
 				//Search through all stocknames to find the right one if it exists
 				//TODO: Make this search more effective! Maybe by adding a new method to JPAHelper that does this
 				for( StockNames st : nameList ) {
-					
+		
 					//If match
 					if( st.getName().equals( _view.getCurrentWantedStock() ) ) {
-						
+		
 						//Create new series
 						final TimeSeries series = new TimeSeries( st.getName() );
-						
+		
 						//Get all of the stock's prices
 						List<StockPrices> priceList = jpaHelper.getPricesForStock( st );
-						
+		
 						//Add all prices through time to the serie
 						for( StockPrices sp : priceList ) {
-							
+		
 							series.addOrUpdate( new Minute( sp.getTime() ), FinancialLongConverter.toDouble( sp.getLatest() ) );
 						}
-						
+		
 						//Insert serie to the view / model of the view
 						_view.insertSeries( series );
 					}
@@ -69,15 +104,35 @@ public class GraphController {
 		};
 	}
 	
-	/**
-	 * Bind a view of type GraphView (tight coupling here) to this controller
-	 * 
-	 * @param view
-	 */
-	public void bindGraphView( GraphView view ) {
+	
+	@Override
+	public List<Pair<String, ActionListener>> getActionListeners() {
 		
-		_view = view; 
+		
+		
+		retList.add( new Pair<String, ActionListener>( BIND_GRAPH_VIEW, bindGraphView() ) );
+		
+		return retList;
 	}
 	
-	public GraphView getView() { return _view; }
+	@Override
+	public void addSubController(IController subController) {
+		
+		//For now, do nothing here since this view does not have sub views
+	}
+	
+	/**
+	 * Get Class Name
+	 */
+	@Override
+	public String getName() {
+		
+		return CLASS_NAME;
+	}
+
+	@Override
+	public void defineSubControllers() {
+		
+		//We have no sub controllers at this level
+	}
 }
--- stockrobot/src/controller/gui/MainMenuController.java
+++ stockrobot/src/controller/gui/MainMenuController.java
@@ -3,30 +3,45 @@ package controller.gui;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.util.ArrayList;
+import java.util.List;
 
-import view.SimulationView;
+import utils.global.Pair;
+import view.MainMenuView;
+/*import view.SimulationView;
 import view.StockInfoGUI;
 import view.StockTableHistoryView;
 import view.graph.GraphView;
-import viewfactory.ViewFactory;
+import viewfactory.ViewFactory;*/
 
 /**
  * 
  * @author kristian
  *
  */
-public class MainMenuController {
+public class MainMenuController implements IController {
 
-	private StockInfoGUI stockInfoGUI;
-	private SimulationView simulationView;
-	private GraphView graphView;
-	private StockTableHistoryView sthw;
+	//private StockInfoGUI stockInfoGUI;
+	//private SimulationView simulationView;
+	//private GraphView _graphView;
+	//private StockTableHistoryView sthw;
+	
+	private static final String CLASS_NAME = "mainMenuController";
+	
+	private static final String OPEN_GRAPH_BUTTON = "openGraphButton";
+	
+	MainMenuView _view;
+	
+	private GraphController graphController;
+	private List<Pair<String, ActionListener>> actionListeners;
 	
 	public MainMenuController() {
 		
-		
+		actionListeners = new ArrayList<Pair<String,ActionListener>>();
 	}
 	
+	/*
 	public void bindStockInfoGUI(StockInfoGUI stockInfoGUI) {
 		
 		this.stockInfoGUI = stockInfoGUI;
@@ -36,23 +51,21 @@ public class MainMenuController {
 		this.simulationView = simulationView;
 	}
 	
-	public void bindGraphView(GraphView graphView) {
-		
-		this.graphView = graphView;
-	}
-	
 	public void bindHistoryView(StockTableHistoryView sthw) {
 		this.sthw = sthw;
 	}
+	*/
 	
 	//Getters for testing
+	/*
 	public StockInfoGUI getStockInfoGUI() { return stockInfoGUI; }
 	
 	public SimulationView getSimulationView() { return simulationView; }
 	
-	public GraphView getGraphView() { return graphView; }
+	public GraphView getGraphView() { return _graphView; }
 	
 	public StockTableHistoryView getStockTableHistoryView() { return sthw; }
+	
 	
 	public ActionListener bindStockInfoGUIButton() {
 		
@@ -66,7 +79,9 @@ public class MainMenuController {
 			}
 		};
 	}
+	*/
 	
+	/*
 	public ActionListener bindAlgorithmSettingsGUIButton() {
 		
 		return  new ActionListener() {
@@ -92,7 +107,7 @@ public class MainMenuController {
 			}
 		};
 	}
-	
+	*/
 	public ActionListener bindOpenGraphButton() {
 		
 		return new ActionListener() {
@@ -100,11 +115,11 @@ public class MainMenuController {
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				//The purpose of this line is just to open a new JFrame
-				graphView.init();
+				
 			}
 		};
 	}
-	
+	/*
 	public ActionListener bindHistoryButton() {
 		
 		return new ActionListener() {
@@ -116,5 +131,63 @@ public class MainMenuController {
 			}
 		};
 	}
+*/
 	
+	
+	//_____________________________________________________________________________//
+	
+	@Override
+	public void propertyChange(PropertyChangeEvent arg0) {
+		
+		
+	}
+
+	//Cascade downwards the tree
+	@Override
+	public void display(Object model) {
+		
+		_view = new MainMenuView();
+		ActionListener openGraphListener = bindOpenGraphButton();
+		actionListeners.add( new Pair<String, ActionListener>(OPEN_GRAPH_BUTTON, openGraphListener));
+		_view.bindOpenGraphView( openGraphListener );
+		
+		_view.init();
+		
+		//For now, call defineSubControllers here
+		defineSubControllers();
+	}
+
+	@Override
+	public void cleanup() {
+		
+		//Cascade downwards the tree
+		graphController.cleanup();
+		graphController = null;
+		
+		_view = null;
+	}
+
+	@Override
+	public List<Pair<String, ActionListener>> getActionListeners() {
+		
+		return null;
+	}
+
+	@Override
+	public void addSubController(IController subController) {
+		
+		
+	}
+
+	@Override
+	public String getName() {
+		
+		return CLASS_NAME;
+	}
+
+	@Override
+	public void defineSubControllers() {
+		
+		graphController = new GraphController();
+	}	
 }
