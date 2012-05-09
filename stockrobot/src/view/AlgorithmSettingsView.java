5c6714628268d2ddbb9ab8c0cb8d692797879ba0
Kristian Sällberg <kristian@purestyle.se>
2012-05-09 16:55:51 +0200

    Transformed AlgorithmSettings to the new MVC structure. But the SubView SettingsPanel yet needs to be transformed

--- stockrobot/src/view/AlgorithmSettingsView.java
+++ stockrobot/src/view/AlgorithmSettingsView.java
@@ -8,6 +8,19 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+import view.algorithmsettings.SettingsPanel;
+
+/**
+ * View that could be used for modifying an algortihm's settings
+ * 
+ * It should load all settings in a specific algorithm and then display some sort of mean to change that
+ * For instance a dragger between 0 and 100
+ * 
+ * TODO: The SettingsPanel is not yet written in an MVC manner. Should be in MVC
+ * 
+ * @author kristian
+ *
+ */
 public class AlgorithmSettingsView extends JFrame implements IView {
 
 	private static final long serialVersionUID = 1L;
@@ -18,13 +31,27 @@ public class AlgorithmSettingsView extends JFrame implements IView {
 	public AlgorithmSettingsView( String algorithmName ) {
 		
 		this.algorithmName = algorithmName;
+		
+		//Main container for the algorithm settings window
+		container = new JPanel();
+		add( container );
+		
+		//Add some settings, a loop just for now
+		for( int i = 0; i < 6; i ++ ) {
+			
+			addSetting( "Setting" + i, Math.round( 20 + (i * 2) ), 0, 99);
+		}
+		
+		//Add save button
+		JButton saveBtn = new JButton();
+		saveBtn.setText( "Save settings" );
+		container.add( saveBtn );
 	}
 	
 	public void addSetting( String desc, double init, double min, double max ) {
 		
-		//JPanel panel = ViewFactory.getSettingsPanel( desc, init, min, max );
-		JButton panel = new JButton("hey");
-		container.add( panel );
+		SettingsPanel panel = new SettingsPanel( desc, init, min, max );
+		container.add(panel);
 	}
 
 	@Override
@@ -36,35 +63,20 @@ public class AlgorithmSettingsView extends JFrame implements IView {
 	@Override
 	public void display(Object model) {
 		
-		//Main container for the algorithm settings window
-		container = new JPanel();
-		add( container );
-		
-		//Add some settings, a loop just for now
-		for( int i = 0; i < 6; i ++ ) {
-			
-			addSetting( "Setting" + i, 20 + (i * 2), 0, 99);
-		}
-		
-		//Add save button
-		JButton saveBtn = new JButton();
-		saveBtn.setText( "Save settings" );
-		container.add( saveBtn );
-		
 		//Settings for the window
 		setTitle( "Algorithm settings for " + algorithmName );
 		setSize( 320,500 );
+		setVisible(true);
 	}
 
 	@Override
 	public void cleanup() {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
 	@Override
 	public void addActions(Map<String, EventListener> actions) {
-		// TODO Auto-generated method stub
 		
 	}
 }
