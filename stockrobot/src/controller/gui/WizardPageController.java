0281fc89fb25c8efd369fa5de63a7392cc9a7731
Mattias <mattias.markehed@gmail.com>
2012-05-10 20:16:47 +0200

    added new uc

---
+++ stockrobot/src/controller/gui/WizardPageController.java
@@ -0,0 +1,24 @@
+package controller.gui;
+
+import java.beans.PropertyChangeEvent;
+import view.wizard.WizardPage;
+
+public abstract class WizardPageController implements IController {
+
+	protected String name = WizardPageController.class.getName();
+	
+	public abstract WizardPage getView();
+	
+	@Override
+	public void propertyChange(PropertyChangeEvent evt) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public String getName() {
+		return name;
+	}
+
+}
