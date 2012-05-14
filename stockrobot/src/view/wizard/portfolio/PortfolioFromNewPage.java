3468745a7a95bff228776159f33065f8d12b3732
Mattias <mattias.markehed@gmail.com>
2012-05-14 11:28:20 +0200

    Added error messages to wizard page

--- stockrobot/src/view/wizard/portfolio/PortfolioFromNewPage.java
+++ stockrobot/src/view/wizard/portfolio/PortfolioFromNewPage.java
@@ -19,11 +19,16 @@ import model.wizard.portfolio.PortfolioWizardModel;
 
 import view.wizard.WizardPage;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Color;
 
 public class PortfolioFromNewPage extends WizardPage {
 
 	private static final long serialVersionUID = 20596476598729363L;
 	private JComboBox cmbAlgorithms;
+	private JLabel lblBalanceError;
+	private JLabel lblErrorAlgorithm;
+	private JTextField txtBalance;
 	
 	public PortfolioFromNewPage(WizardModel wizardModel, PortfolioWizardModel pageModel) {
 		super(wizardModel, pageModel);
@@ -33,24 +38,37 @@ public class PortfolioFromNewPage extends WizardPage {
 		this.setAlignmentX(Component.LEFT_ALIGNMENT);
 		BoxLayout lou_PortfolioInfo = new BoxLayout(this, BoxLayout.PAGE_AXIS);
 		this.setLayout(lou_PortfolioInfo);
-		this.setPreferredSize(new Dimension(287, 81));
+		this.setPreferredSize(new Dimension(541, 81));
 		
 		JPanel pnl_balance = new JPanel();
+		FlowLayout flowLayout_1 = (FlowLayout) pnl_balance.getLayout();
+		flowLayout_1.setAlignment(FlowLayout.LEFT);
 		JLabel lbl_balance = new JLabel("Balance");
 		pnl_balance.add(lbl_balance);
-		JTextField txt_balance = new JTextField();
-		txt_balance.setColumns(15);
-		pnl_balance.add(txt_balance);
+		txtBalance = new JTextField();
+		txtBalance.setColumns(15);
+		pnl_balance.add(txtBalance);
 		this.add(pnl_balance);
+		
+		lblBalanceError = new JLabel("Error");
+		lblBalanceError.setVisible(false);
+		lblBalanceError.setForeground(Color.RED);
+		pnl_balance.add(lblBalanceError);
 		
 		List<String> algorithms = PluginAlgortihmLoader.getInstance().getAlgorithmNames();
 		
 		JPanel pnlAlgorithm = new JPanel();
+		FlowLayout flowLayout = (FlowLayout) pnlAlgorithm.getLayout();
+		flowLayout.setAlignment(FlowLayout.LEFT);
 		add(pnlAlgorithm);
 		JLabel lblAlgorithmName = new JLabel("Algorithm Name:");
 		pnlAlgorithm.add(lblAlgorithmName);
 		cmbAlgorithms = new JComboBox(algorithms.toArray());
 		pnlAlgorithm.add(cmbAlgorithms);
+		
+		lblErrorAlgorithm = new JLabel("Error");
+		lblErrorAlgorithm.setForeground(Color.RED);
+		pnlAlgorithm.add(lblErrorAlgorithm);
 		//========================================
 		
 		
