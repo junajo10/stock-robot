27b5c76c35a507a59676b3ca2229ad82f68652de
erikramqvist <erikramqvist@gmail.com>
2012-05-18 14:59:42 +0200

    Change Log to work as a Singleton. Also added ListModel for AstroLog.

--- stockrobot/src/utils/global/Log.java
+++ stockrobot/src/utils/global/Log.java
@@ -2,6 +2,8 @@ package utils.global;
 
 import java.util.HashMap;
 
+import javax.swing.DefaultListModel;
+
 /**
  * @author Mattias Markehed
  * mattias.markehed@gmail.com
@@ -25,6 +27,7 @@ public final class Log {
 	
 	private HashMap<TAG, Boolean> filter;
 	private HashMap<TAG, String> shortenerMap;
+	private DefaultListModel listModel;
 	
 	private Log() {
 		initialize();
@@ -32,14 +35,24 @@ public final class Log {
 	
 	public static void log(TAG tag, String message){
 	
-		if(log.filter.get(tag)){
+		instance().addLogMessage(message);
+		/*if(log.filter.get(tag)){
 			System.out.print("[" + log.shortenerMap.get(tag) + "] " ); //NOPMD
 			System.out.print(message); //NOPMD
 			System.out.println(); //NOPMD
-		}
+		}*/
 	}
 	
+	private void addLogMessage(String message) {
+		listModel.addElement(message);
+	}
+	
+	public DefaultListModel getModel(){
+		return listModel;
+	}
+
 	private void initialize(){
+		listModel = new DefaultListModel();
 		filter=new HashMap<Log.TAG, Boolean>();
 		filter.put(TAG.NORMAL, true);
 		filter.put(TAG.VERBOSE, false);
@@ -81,8 +94,7 @@ public final class Log {
 		}
 		
 	}
-	
-	@Deprecated
+
 	public static Log instance(){
 		
 		return log;
--- stockrobot/src/view/AstroView.java
+++ stockrobot/src/view/AstroView.java
@@ -72,7 +72,7 @@ public class AstroView extends JFrame implements IView {
 	public AstroView() {
 		setResizable(false);
 		setTitle("ASTRo Main");
-		setBounds(100, 100, 213, 396);
+		setBounds(100, 100, 296, 396);
 		
 		JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
@@ -105,19 +105,23 @@ public class AstroView extends JFrame implements IView {
 
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
 		gl_contentPane.setHorizontalGroup(
-			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
+			gl_contentPane.createParallelGroup(Alignment.TRAILING)
+				.addGroup(gl_contentPane.createSequentialGroup()
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
-						.addComponent(scrollPane, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 180, Short.MAX_VALUE)
+						.addComponent(scrollPane, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 270, Short.MAX_VALUE)
 						.addGroup(Alignment.LEADING, gl_contentPane.createSequentialGroup()
 							.addComponent(lblAstroStatus)
 							.addPreferredGap(ComponentPlacement.UNRELATED)
-							.addComponent(progressBar, GroupLayout.PREFERRED_SIZE, 100, GroupLayout.PREFERRED_SIZE))
-						.addComponent(btnPortfolio, GroupLayout.DEFAULT_SIZE, 180, Short.MAX_VALUE)
-						.addComponent(btnStocks, GroupLayout.DEFAULT_SIZE, 180, Short.MAX_VALUE)
-						.addComponent(btnSimulate, GroupLayout.DEFAULT_SIZE, 180, Short.MAX_VALUE)
-						.addComponent(btnGraph, GroupLayout.DEFAULT_SIZE, 180, Short.MAX_VALUE))
-					.addGap(35))
+							.addComponent(progressBar, GroupLayout.DEFAULT_SIZE, 190, Short.MAX_VALUE))
+						.addGroup(Alignment.LEADING, gl_contentPane.createSequentialGroup()
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+								.addComponent(btnSimulate, GroupLayout.PREFERRED_SIZE, 131, GroupLayout.PREFERRED_SIZE)
+								.addComponent(btnPortfolio, GroupLayout.PREFERRED_SIZE, 127, GroupLayout.PREFERRED_SIZE))
+							.addPreferredGap(ComponentPlacement.UNRELATED)
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
+								.addComponent(btnGraph, GroupLayout.DEFAULT_SIZE, 145, Short.MAX_VALUE)
+								.addComponent(btnStocks, GroupLayout.DEFAULT_SIZE, 133, Short.MAX_VALUE))))
+					.addContainerGap())
 		);
 		gl_contentPane.setVerticalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
@@ -126,15 +130,15 @@ public class AstroView extends JFrame implements IView {
 						.addComponent(lblAstroStatus)
 						.addComponent(progressBar, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
 					.addPreferredGap(ComponentPlacement.UNRELATED)
-					.addComponent(btnPortfolio)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(btnPortfolio)
+						.addComponent(btnStocks))
 					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(btnStocks)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(btnSimulate)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(btnGraph)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 224, Short.MAX_VALUE)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(btnSimulate)
+						.addComponent(btnGraph))
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 238, Short.MAX_VALUE)
 					.addContainerGap())
 		);
 		
